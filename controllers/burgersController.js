c8ce6f35c7822c4b1e7a7e6c94907e47f07f290c
Joy Tempkins <jtempkins@gmail.com>
2019-10-28 16:08:26 -0500

    initial upload

---
+++ controllers/burgersController.js
@@ -0,0 +1,52 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    console.log(data);
+    res.render("index", {bunObject:data});
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne([
+    "name", "devoured"
+  ], [
+    req.body.name, req.body.devour
+  ], function(result) {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devour: true
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+module.exports = router;
